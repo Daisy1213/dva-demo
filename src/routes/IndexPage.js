4821928cf09df049505bc20c78352e5bd1682d86
zhangs2 <sha.zhang@dianrong.com>
2018-04-25 18:39:22 +0800

    update code

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,38 +2,36 @@ import React from 'react';
 import styles from './IndexPage.css';
 import {connect} from 'dva';
 
-class IndexPage extends React.Component {
-  render() {
-      console.log(this.props);
-      const {count, dispatch} = this.props;
-    return (
-      <div className={styles.normal}>
-        <div className={styles.record}>Highest Record: {count.record}</div>
-        <div className={styles.current}>{count.current}</div>
-        <div className={styles.button}>
-          <button onClick={() => {
-            dispatch({type: 'count/add'})
-          }}>+
-          </button>
-        </div>
-      </div>
-    )
-  }
-}
-
-// function IndexPage() {
-//   console.log(this.props);
-//   const {count, dispatch} = this.props;
-//   return (
-//     <div className={styles.normal}>
-//       <div className={styles.record}>Highest Record: {count.record}</div>
-//       <div className={styles.current}>{count.current}</div>
-//       <div className={styles.button}>
-//         <button onClick={() => {dispatch({type: 'count/add'})}}>+</button>
+// class IndexPage extends React.Component {
+//   render() {
+//       console.log(this.props);
+//       const {count, dispatch} = this.props;
+//     return (
+//       <div className={styles.normal}>
+//         <div className={styles.record}>Highest Record: {count.record}</div>
+//         <div className={styles.current}>{count.current}</div>
+//         <div className={styles.button}>
+//           <button onClick={() => {
+//             dispatch({type: 'count/add'})
+//           }}>+
+//           </button>
+//         </div>
 //       </div>
-//     </div>
-//   );
+//     )
+//   }
 // }
+
+function IndexPage({count, addCount}) {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.record}>Highest Record: {count.record}</div>
+      <div className={styles.current}>{count.current}</div>
+      <div className={styles.button}>
+        <button onClick={addCount}>+</button>
+      </div>
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -41,10 +39,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (disptcha) => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    addCount: disptcha({type: 'count/add'})
+    addCount: () => dispatch({type: 'count/add'})
   };
 };
 
+// export default connect(mapStateToProps)(IndexPage);
 export default connect(mapStateToProps, mapDispatchToProps)(IndexPage);
